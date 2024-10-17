961c951f63a1ab641b92d4ed9fdbd48944fb3b8f
Paula Rodríguez <41522193+paurrodri@users.noreply.github.com>
2024-10-17 14:06:11 +0200

    Update workflow.R

--- inst/scripts/workflow.R
+++ inst/scripts/workflow.R
@@ -2,56 +2,70 @@
 ###  Microbiome trajectory workflow  ###
 ########################################
 
-# Load libraries ----------------------------------------------------------
+# Load libraries ---------------------------------------------------------------
 library(magrittr)
 
-# Set parameters ----------------------------------------------------------
+# Set parameters ---------------------------------------------------------------
 model <- "genus10"
 # model <- "mgs20"
 
-# event <- "antibiotics"
-event <- "rti_mild"
+subset <- "all" # all infants (HMG-CS already excluded)
+# subset <- "C-section" # formula-fed CS infants, AND reference HMG-VD infants
 
-filter_expression <- TRUE # for all infants (HMG-CS already excluded)
-# filter_expression <- expression(Feeding_group == "HMG" | Delivery_mode == "Caesarean") # for formula-fed CS infants, AND reference HMG-VD infants
-
-filter_expression_ff <- expression(Feeding_group != "HMG") # for formula-fed infants
-# filter_expression_ff <- expression(Feeding_group != "HMG" & Delivery_mode == "Caesarean") # for formula-fed CS infants
-
+# Process parameters -----------------------------------------------------------
+if (subset == "all"){
+  filter_expression <- TRUE # for all infants (HMG-CS already excluded)
+  filter_expression_ff <- expression(Feeding_group != "HMG") # for formula-fed infants
+} else if (subset == "C-section"){
+  filter_expression <- expression(Feeding_group == "HMG" | Delivery_mode == "Caesarean") # for formula-fed CS infants, AND reference HMG-VD infants
+  filter_expression_ff <- expression(Feeding_group != "HMG" & Delivery_mode == "Caesarean") # for formula-fed CS infants
+}
 
 # Read input data --------------------------------------------------------------
 microbiota_age_data <- readRDS(glue::glue("../data/microbiota_age_{model}.rds"))
 microbiome_data     <- readRDS(glue::glue("../data/{model}.rds"))
 biomarker_data      <- readRDS(glue::glue("../data/biomarker.rds"))
-event_data          <- readRDS(glue::glue("../data/{event}.rds"))
+antibiotics_data    <- readRDS(glue::glue("../data/antibiotics.rds"))
+rti_mild_data       <- readRDS(glue::glue("../data/rti_mild.rds"))
 metadata            <- readRDS(glue::glue("../data/metadata.rds"))
 
 
-# Calculate MAZ and trajectory status -------------------------------------
+# Calculate MAZ and trajectory status ------------------------------------------
 maz_data               <- calculate_MAZ(microbiota_age_data)
 on_off_trajectory_data <- define_on_off_trajectory(maz_data)
+figure_maz             <- plot_maz(maz_data, filter_expression)
 
 
-# Trajectory and trajectory comparison figures ---------------------------------
+# Microbiota trajectories  -----------------------------------------------------
 figure_trajectories <- plot_trajectories(microbiota_age_data, filter_expression)
 figure_pvals        <- compare_trajectories_between_groups(microbiota_age_data, filter_expression)
-figure_distance     <- calculate_distance_between_trajectories(microbiota_age_data, filter_expression)
 
 
 # On/off-trajectory vs feeding group comparison  -------------------------------
-on_off_trend_test_results <- compare_on_off_trajectory_between_groups_all_combinations(on_off_trajectory_data, filter_expression)
+on_off_trend_test_results  <- compare_on_off_trajectory_between_groups_all_combinations(on_off_trajectory_data, filter_expression)
+figure_on_off_groups_early <- plot_on_off_trajectory_between_groups(on_off_trajectory_data, filter_expression, TG_group = "TGs", trajectory_status_variable = "Trajectory_status_early_visits", label_on_trajectory = "On-trajectory 3-6 months", label_off_trajectory = "Off-trajectory 3-6 months")
+figure_on_off_groups_late  <- plot_on_off_trajectory_between_groups(on_off_trajectory_data, filter_expression, TG_group = "TGs", trajectory_status_variable = "Trajectory_status_late_visits", label_on_trajectory = "On-trajectory 12-15 months", label_off_trajectory = "Off-trajectory 12-15 months")
 
 
 # Microbiome taxa, metabolic biomarkers ----------------------------------------
 microbiome_trajectory_status_comparison <- compare_biome_between_on_off_trajectory(microbiome_data, on_off_trajectory_data, filter_expression_ff)
 biomarker_trajectory_status_comparison  <- compare_biome_between_on_off_trajectory(biomarker_data,  on_off_trajectory_data, filter_expression_ff)
 microbiome_biomarker_correlation        <- correlate_microbiome_biomarkers(microbiome_data, biomarker_data, metadata, filter_expression_ff)
+# figure butyrate
+# figure propionate
+# all biomarkers_list
 
 
 # Clinical outcomes ------------------------------------------------------------
-cox_ph_results       <- perform_cox_ph_test_all_combinations(event_data, on_off_trajectory_data, filter_expression_ff)
-kaplan_meier_figure  <- plot_kaplan_meier(event_data, on_off_trajectory_data, filter_expression_ff) # # Figure from Main (either antibiotics or mild RTI)
-kaplan_meier_figures <- plot_kaplan_meier_all_combinations(event_data, event, on_off_trajectory_data, filter_expression_ff) # All 3 figures (Main and ED, either antibiotics or mild RTI)
+figure_kaplan_meier_antibiotics   <- plot_kaplan_meier(antibiotics_data, on_off_trajectory_data, filter_expression_ff) # Figure from Main
+figure_kaplan_meier_rti_mild      <- plot_kaplan_meier(rti_mild_data,    on_off_trajectory_data, filter_expression_ff) # Figure from Main
 
+figures_kaplan_meier_antibiotics  <- plot_kaplan_meier_all_combinations(antibiotics_data, event, on_off_trajectory_data, filter_expression_ff) # All 3 figures (Main and Extended)
+figures_kaplan_meier_rti_mild     <- plot_kaplan_meier_all_combinations(rti_mild_data,    event, on_off_trajectory_data, filter_expression_ff) # All 3 figures (Main and Extended)
 
+cox_ph_results_antibiotics        <- perform_cox_ph_test_all_combinations(antibiotics_data, on_off_trajectory_data, filter_expression_ff)
+cox_ph_results_rti_mild           <- perform_cox_ph_test_all_combinations(rti_mild_data,    on_off_trajectory_data, filter_expression_ff)
+
+figure_cox_ph_antibiotics         <- plot_forest_plot_cox_ph(cox_ph_results_antibiotics)
+figure_cox_ph_rti_mild            <- plot_forest_plot_cox_ph(cox_ph_results_rti_mild)
 
